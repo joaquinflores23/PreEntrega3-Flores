6e44a3bbe8387abc1c960a11cc781984a2d3704f
joaquinflores23 <joakoflores@hotmail.com>
2023-08-21 22:01:48 -0300

    mejor formato alt images

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ const generarTarjetas = array => {
     <div class="col mb-5">
         <div class="card h-100">
             <!-- Product image-->
-            <img class="card-img-top" src=${ifImgNotFound} alt=${element.producto} />
+            <img class="card-img-top" src=${ifImgNotFound} alt="Imagen ${element.producto}" />
             <!-- Product details-->
             <div class="card-body p-4">
                 <div class="text-center">
